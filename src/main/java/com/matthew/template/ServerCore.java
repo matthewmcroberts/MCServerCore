81ee6888868ad2c797f6f6cd73952d200387276d
Matthew <matthew.mcroberts22@gmail.com>
2024-04-05 18:41:23 -0400

    previously uploaded wrong main file

--- src/main/java/com/matthew/template/ServerCore.java
+++ src/main/java/com/matthew/template/ServerCore.java
@@ -1,54 +1,42 @@
 package com.matthew.template;
 
-import com.matthew.template.api.ServerModule;
-import com.matthew.template.commands.FireCommand;
+import com.matthew.template.generalcommands.GeneralCommandManager;
+import com.matthew.template.generalevents.GeneralEventsManager;
 import com.matthew.template.modules.manager.ServerModuleManager;
 import com.matthew.template.modules.permissions.PermissionsModule;
-import com.matthew.template.commands.SpawnEntityCommand;
-import com.matthew.template.events.CowListener;
-import com.matthew.template.events.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
 
 public final class ServerCore extends JavaPlugin {
 
-    private static ServerCore instance;
-
     private ServerModuleManager moduleManager;
+    private GeneralEventsManager mechanicManager;
+    private GeneralCommandManager commandManager;
+
 
     @Override
     public void onEnable() {
-        instance = this;
-        registerCommands();
-        registerListeners();
+
+        //General Events Setup
+        mechanicManager = new GeneralEventsManager(this);
+        mechanicManager.register();
+
+        //General Commands Setup
+        commandManager = new GeneralCommandManager(this);
+        commandManager.register();
+
+        //Modules Setup
         moduleManager = ServerModuleManager.getInstance();
         moduleManager.registerModule(new PermissionsModule(this));
-        for(ServerModule module: moduleManager.getRegisteredModules()) {
-            module.setUp();
-        }
+        moduleManager.setUp();
 
         getLogger().info("NativePractice loaded");
     }
 
     @Override
     public void onDisable() {
-        for(ServerModule module: moduleManager.getRegisteredModules()) {
-            module.teardown();
-        }
-    }
-
-    private void registerCommands() {
-        Objects.requireNonNull(getCommand("spawnentity")).setExecutor(new SpawnEntityCommand());
-        Objects.requireNonNull(getCommand("fire")).setExecutor(new FireCommand());
-    }
-
-    private void registerListeners() {
-        getServer().getPluginManager().registerEvents(PlayerListener.getInstance(), this);
-        getServer().getPluginManager().registerEvents(new CowListener(), this);
-    }
-
-    public static ServerCore getInstance() {
-        return instance;
+        moduleManager.teardown();
+        mechanicManager.unregister();
+        commandManager.unregister();
     }
 }
