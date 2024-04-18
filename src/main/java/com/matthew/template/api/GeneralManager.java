70f67198700f889d1018d894bf53fd019271113d
Matthew <matthew.mcroberts22@gmail.com>
2024-04-17 21:12:54 -0400

    adding annotations

--- src/main/java/com/matthew/template/api/GeneralManager.java
+++ src/main/java/com/matthew/template/api/GeneralManager.java
@@ -1,8 +1,12 @@
 package com.matthew.template.api;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface GeneralManager {
 
+    @NotNull
     void register();
 
+    @NotNull
     void unregister();
 }
--- src/main/java/com/matthew/template/api/ServerModule.java
+++ src/main/java/com/matthew/template/api/ServerModule.java
@@ -1,8 +1,12 @@
 package com.matthew.template.api;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface ServerModule {
 
+    @NotNull
     void setUp();
 
+    @NotNull
     void teardown();
 }
