c1954c674151e6a138658e77fe08133bc555c7a0
Matthew <matthew.mcroberts22@gmail.com>
2024-05-01 13:14:01 -0400

    added comments and modified to return copy of collections in cache

--- src/main/java/com/matthew/template/modules/storage/DataStorageModule.java
+++ src/main/java/com/matthew/template/modules/storage/DataStorageModule.java
@@ -8,75 +8,133 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
 
-/*
- The DataStorageModule's purpose is for getting and adding data directly stored in the Cache
- For additional functionality with the data, the other modules will be accessed
+/**
+ * The DataStorageModule class handles the storage of ranks and player data for the plugin.
+ * It provides methods to add, remove, and retrieve ranks and player data. For additional usage with data stored in
+ * cache, the other modules are to be used.
  */
 public final class DataStorageModule implements ServerModule {
 
+    /*
+     * The JavaPlugin the ServerModule is created from
+     */
     private JavaPlugin plugin;
-    private final Cache cache = new Cache();
 
+    /*
+     * The cache storing all necessary data related to the players and ranks
+     */
+    private final Cache cache;
+
+    /**
+     * Constructs a new DataStorageModule with the given JavaPlugin instance.
+     *
+     * @param plugin The JavaPlugin instance associated with this module.
+     */
     public DataStorageModule(JavaPlugin plugin) {
         this.plugin = plugin;
+        this.cache = new Cache();
     }
 
+    /**
+     * Adds a new rank to cache.
+     *
+     * @param rank The Rank object to add.
+     * @return True if the rank was successfully added, false otherwise.
+     */
     public boolean addRank(Rank rank) {
-        if ((contains(rank))) {
+        if ((containsRank(rank))) {
             return false;
         }
         cache.getRanks().add(rank);
         return true;
     }
 
+    /**
+     * Removes a rank from cache.
+     *
+     * @param rank The Rank object to remove.
+     * @return True if the rank was successfully removed, false otherwise.
+     */
     public boolean removeRank(Rank rank) {
-        if (!(contains(rank))) {
+        if (!(containsRank(rank))) {
             return false;
         }
         cache.getRanks().remove(rank);
         return true;
     }
-    //TODO: Return a copy of set instead
+
+    /**
+     * Retrieves all ranks stored in cache.
+     *
+     * @return An unmodifiable set of all ranks.
+     */
     public Set<Rank> getAllRanks() {
-        return cache.getRanks();
+        return Collections.unmodifiableSet(cache.getRanks());
     }
 
-
+    /**
+     * Adds a player's data to cache.
+     *
+     * @param player The PlayerData object to add.
+     * @return True if the player's data was successfully added, false otherwise.
+     */
     public boolean addPlayer(PlayerData player) {
-        if ((contains(player))) {
+        if ((containsPlayer(player))) {
             return false;
         }
         cache.getPlayers().add(player);
         return true;
     }
 
+    /**
+     * Removes a player's data from cache.
+     *
+     * @param player The PlayerData object to remove.
+     * @return True if the player's data was successfully removed, false otherwise.
+     */
     public boolean removePlayer(PlayerData player) {
-        if(!(contains(player))) {
+        if(!(containsPlayer(player))) {
             return false;
         }
         cache.getPlayers().remove(player);
         return true;
     }
 
-    //TODO: Return a copy of list instead
+    /**
+     * Retrieves all player data stored in the module.
+     *
+     * @return An unmodifiable list of all player data.
+     */
     public List<PlayerData> getAllPlayerData() {
-        return cache.getPlayers();
+        return Collections.unmodifiableList(cache.getPlayers());
     }
 
-    public boolean contains(Rank rank) {
+    /**
+     * Checks if a rank is contained in cache.
+     *
+     * @param rank The Rank object to check.
+     * @return True if the rank is contained, false otherwise.
+     */
+    public boolean containsRank(Rank rank) {
         return cache.getRanks().contains(rank);
     }
 
-    public boolean contains(PlayerData playerData) {
+    /**
+     * Checks if a player's data is contained in cache.
+     *
+     * @param playerData The PlayerData object to check.
+     * @return True if the player's data is contained, false otherwise.
+     */
+    public boolean containsPlayer(PlayerData playerData) {
         return cache.getPlayers().contains(playerData);
     }
 
-    private void clearRanks() {
-        cache.getRanks().clear();
-    }
-
-    private void clearPlayers() { cache.getPlayers().clear(); }
-
+    /**
+     * Checks if a player is contained in cache by name.
+     *
+     * @param playerData The Player object to check.
+     * @return True if the player is contained, false otherwise.
+     */
     private boolean contains(Player playerData) {
         for(PlayerData player: cache.getPlayers()) {
             if(player.getName().equals(playerData.getName())) {
@@ -86,33 +144,62 @@ public final class DataStorageModule implements ServerModule {
         return false;
     }
 
-
+    /**
+     * Sets up the module. (Currently empty)
+     */
     @Override
     public void setUp() {
 
     }
 
+    /**
+     * Tears down any additional allocated resources
+     */
     @Override
     public void teardown() {
-        clearRanks();
-        clearPlayers();
+        cache.clear();
     }
 
+    /**
+     * The Cache class is an internal class for storing ranks and player data. The purpose is to provide a tight
+     * coupling between the Cache and DataStorageModule
+     */
     private static class Cache {
         private final Set<Rank> ranks;
         private final List<PlayerData> players;
 
+        /**
+         * Constructs a new Cache with an empty set and list.
+         */
         public Cache() {
             this.ranks = new HashSet<>();
             this.players = new ArrayList<>();
         }
 
+        /**
+         * Retrieves the set of ranks.
+         *
+         * @return The set of ranks.
+         */
         public Set<Rank> getRanks() {
             return ranks;
         }
 
+        /**
+         * Retrieves the list of player data.
+         *
+         * @return The list of player data.
+         */
         public List<PlayerData> getPlayers() {
             return players;
+        }
+
+        /**
+         * Clears all stored data.
+         */
+        public void clear() {
+            ranks.clear();
+            players.clear();
         }
     }
 }
