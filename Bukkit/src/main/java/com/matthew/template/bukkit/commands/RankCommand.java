50410544d0be12ccd1e127013e97e47ca56079b9
Matthew <matthew.mcroberts22@gmail.com>
2024-08-07 15:11:35 -0400

    working on rank command system

--- Bukkit/src/main/java/com/matthew/template/bukkit/commands/RankCommand.java
+++ Bukkit/src/main/java/com/matthew/template/bukkit/commands/RankCommand.java
@@ -1,7 +1,13 @@
 package com.matthew.template.bukkit.commands;
 
 import com.matthew.template.bukkit.annotations.RegisterCommand;
+import com.matthew.template.common.modules.manager.ServerModuleManager;
+import com.matthew.template.common.modules.player.PlayerModule;
+import com.matthew.template.common.modules.player.data.PlayerData;
+import com.matthew.template.common.modules.ranks.RankModule;
+import com.matthew.template.common.modules.ranks.data.RankData;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -10,10 +16,23 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 @RegisterCommand(name = "rank")
 public class RankCommand implements TabExecutor {
+
+    private PlayerModule playerModule = ServerModuleManager.getInstance().getRegisteredModule(PlayerModule.class);
+
+    private RankModule rankModule = ServerModuleManager.getInstance().getRegisteredModule(RankModule.class);
+
+    private final Map<String, Consumer<PlayerData>> singleArgCommandActions = new HashMap<>();
+
+    private final Map<String, BiConsumer<PlayerData, RankData>> doubleArgCommandActions = new HashMap<>();
+
 
     public RankCommand() {
     }
@@ -27,7 +46,49 @@ public class RankCommand implements TabExecutor {
             return true;
         }
 
-        player.sendMessage("Fired");
+        PlayerData playerData = playerModule.getPlayerData(player);
+
+        if (playerData == null) {
+            Bukkit.getLogger().warning("Unexpected outcome occurred while attempting to execute RankCommand: '"
+                    + player.getName() + "' (sender) not found in PlayerData cache.");
+            return true;
+        }
+
+        if(!player.hasPermission("rankcommand.use")) {
+            player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            return true;
+        }
+
+        if(args.length != 2) {
+            player.sendMessage(ChatColor.RED + "Incorrect usage");
+            return true;
+        }
+
+        String actionArg = args[0].toLowerCase();
+
+
+        if(!"get".equals(actionArg)) {
+
+            Player target = Bukkit.getPlayer(args[1]);
+
+            if(target == null) {
+                player.sendMessage(ChatColor.RED + "Player '" + args[1] + "' not found");
+                return true;
+            }
+
+            Consumer<PlayerData> action = singleArgCommandActions.get(actionArg);
+
+            if(action == null) {
+                Bukkit.getLogger().severe("Missing action for RankCommand'" + actionArg + "'");
+                return true;
+            }
+
+            action.accept(playerData);
+
+        } else {
+
+        }
+
         return true;
     }
 
@@ -35,5 +96,15 @@ public class RankCommand implements TabExecutor {
     @Override
     public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         return new ArrayList<>();
+    }
+
+    public void registerActions() {
+        doubleArgCommandActions.put("set", (player, rank) -> {
+
+        });
+
+        singleArgCommandActions.put("get", (player) -> {
+
+        });
     }
 }
