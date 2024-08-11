324cc3654f1eec06f009402af3702f187cd1f220
Matthew <matthew.mcroberts22@gmail.com>
2024-08-11 01:06:58 -0400

    working on message module

--- Bukkit/src/main/java/com/matthew/template/bukkit/modules/messages/MessageModule.java
+++ Bukkit/src/main/java/com/matthew/template/bukkit/modules/messages/MessageModule.java
@@ -1,2 +1,91 @@
-package com.matthew.template.bukkit.modules.messages;public class MessageModule {
+package com.matthew.template.bukkit.modules.messages;
+
+import com.matthew.template.common.apis.ServerModule;
+import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+public class MessageModule implements ServerModule {
+
+    private final JavaPlugin plugin;
+    private Map<String, String> cache;
+    private String prefix;
+
+    public String buildMessage(String key, Object... args) {
+        String message = cache.get(key);
+
+        if (message == null) {
+            plugin.getLogger().warning("Message key '" + key + "' not found");
+            return "";
+        }
+
+        message = ChatColor.translateAlternateColorCodes('&', message);
+
+        if ((args != null) && (args.length > 0)) {
+            for (int i = 0; i < args.length; i++) {
+                message = message.replace("<" + i + ">", String.valueOf(args[i]));
+            }
+        }
+        return prefix + message;
+    }
+
+    public void buildThenRunMessage(String key, Consumer<String> callback) {
+        callback.accept(this.buildMessage(key, (Object) null));
+    }
+
+    public void buildThenRunMessage(String key, Consumer<String> callback, Object... args) {
+        callback.accept(this.buildMessage(key, args));
+    }
+
+    public void sendMessage(Player player, String key, Object... args) {
+        String message = buildMessage(key, args);
+        player.sendMessage(message);
+    }
+
+    @Override
+    public void setUp() {
+        File configFile = new File(plugin.getDataFolder(), "messages.yml");
+
+        if (!configFile.exists()) {
+            try (InputStream inputStream = plugin.getResource("messages.yml")) {
+                if (inputStream != null) {
+                    Files.copy(inputStream, configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    plugin.getLogger().info("Copied messages.yml from resources to plugin folder.");
+                } else {
+                    plugin.getLogger().warning("Default messages.yml not found in resources.");
+                }
+            } catch (IOException e) {
+                plugin.getLogger().warning("Error copying messages.yml from resources: " + e.getMessage());
+            }
+        }
+
+        try {
+            Yaml yaml = new Yaml();
+            cache = yaml.load(Files.newBufferedReader(configFile.toPath()));
+            prefix = cache.getOrDefault("prefix", ""); // Load the prefix from messages.yml
+            prefix = ChatColor.translateAlternateColorCodes('&', prefix);
+        } catch (IOException e) {
+            plugin.getLogger().warning("Error loading messages.yml: " + e.getMessage());
+        }
+
+        if (cache != null) {
+            plugin.getLogger().info("Loaded messages from messages.yml: " + cache.keySet());
+        }
+    }
+
+    @Override
+    public void teardown() {
+        cache.clear();
+    }
 }
